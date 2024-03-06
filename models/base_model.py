126ff8faa769bca0a03e1a877bb0c827806bbb5b
AbduHany <abdu.hany@gmail.com>
2024-03-06 17:52:13 +0200

    Add FileStorage class & Modify base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """
 import uuid
 import datetime
+from models import storage
 
 
 class BaseModel:
@@ -14,11 +15,16 @@ class BaseModel:
         Assigns id attribute with uuid4(), created_at with the
         datetime object when the instance is created, and
         updated_at with the datetime object when the instance is updated.
+        Args:
+            args (non-keyworded arguments): not used in the function.
+            kwargs (keyworded-arguments dictionary): The arguments passed
+                as dictionary to create a new instance.
         """
-        if not kwargs:
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -39,9 +45,10 @@ class BaseModel:
 
     def save(self):
         """ This method updates the updated_at attribute with the current
-        datetime object.
+        datetime object & saves the objects/instances into a JSON file.
         """
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ This method returns a dictionary containing all keys/values
