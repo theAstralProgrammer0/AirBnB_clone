5504accbb39b0318e8234fc7ed0009d39c602935
AbduHany <abdu.hany@gmail.com>
2024-03-07 00:35:15 +0200

    Add show & create commands to console

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ This program contains the entry point of the command interpreter which serves
 as the front-end for testing and prototyping the backend
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,9 +14,6 @@ class HBNBCommand(cmd.Cmd):
     perpetually
     """
     prompt = "(hbnb) "
-#    intro = "Welcome to Console"
-
-#    doc_header = 'These are the commands available'
 
     def emptyline(self):
         """
@@ -32,6 +31,43 @@ class HBNBCommand(cmd.Cmd):
         """
         print()
         return True
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id
+        """
+        if line == "":
+            print("** class name missing **")
+        elif line == "BaseModel":
+            a = BaseModel()
+            a.save()
+            print(a.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the
+        class name and id
+        """
+        args = line.split()
+        objdict = storage.all()
+        classes_set = set()
+        ids_set = set()
+        for key, value in objdict.items():
+            classes_set.add(value.__class__.__name__)
+            ids_set.add(value.id)
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classes_set:
+            print("** class doesn't exist **")
+        elif (args[0] in classes_set) and (len(args) == 1):
+            print("** instance id missing **")
+        elif (args[0] in classes_set) and (args[1] in ids_set):
+            print(objdict["{}.{}".format(args[0], args[1])])
+        else:
+            print("** no instance found **")
+
 
 
 if __name__ == '__main__':
